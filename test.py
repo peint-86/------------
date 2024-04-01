ffc02e02cbb4f1fdbca45568c1c75afd512e6c63
hulo <reo.harada1019@gmail.com>
2024-04-01 23:07:40 +0900

    いったん終了

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-import pandas as pd
-score = pd.read_csv('exam_score.csv')
-print(score.head(5))
-print(score.describe())
-
+import numpy as np
+def lec_word_matrix(courses, vocab):
+    lec_word=np.zeros((len(courses),len(vocab)),dtype=int)
+    for i in range(len(courses)):
+        for j in range(len(vocab)):
+            if vocab[j] in courses:
+                lec_word[i][j]=1
+    print(lec_word)
+courses = list(open("course_list.csv"))
+vocab = list(open("keyword_list.csv"))
+lec_word_matrix(courses, vocab)
+    
